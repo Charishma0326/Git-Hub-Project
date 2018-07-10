37c4b8af740c343d4a525473597522040eebe860
Charishma0326 <41051485+Charishma0326@users.noreply.github.com>
2018-07-10 15:55:51 +0530

    Add files via upload

---
+++ ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  Git Hub Project
+//
+//  Created by Media3 on 7/10/18.
+//  Copyright © 2018 Media3. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"This is a sample git hub project");
+    NSLog(@"Hello World");
+    NSLog(@"I am learning how to push my xcode project to GitHub");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
